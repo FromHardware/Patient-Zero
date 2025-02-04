2ea8cdbb41f1911b1bc0b341f4b71d9aa85cdd6d
Timothé.O <timothe.ocket@my-digital-school.org>
2025-02-04 11:32:04 +0100

    Learning Menu and Basic Character

---
+++ lejeu/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	# get_tree().change_scene_to_file()
+	pass
+
+func _on_option_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
